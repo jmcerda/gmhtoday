a47bff7f03af27e98ee87dfa9d2c1f043d33a2ce
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-01-19 01:19:34 -0800

    gulp

--- themes/custom/GMH/gulpfile.js
+++ themes/custom/GMH/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
+var sourcemaps = require('gulp-sourcemaps');
 
 var sassPaths = [
   'bower_components/foundation-sites/scss',
@@ -7,15 +8,21 @@ var sassPaths = [
 ];
 
 gulp.task('sass', function() {
-  return gulp.src('scss/**/*.scss')
+  return gulp
+    .src('scss/**/*.scss')
     .pipe($.sass({
       includePaths: sassPaths
     })
-      .on('error', $.sass.logError))
+    .on('error', $.sass.logError))
+    .pipe(sourcemaps.write())
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
     .pipe(gulp.dest('css'));
+    pipe(sassdoc())
+    // Release the pressure back and trigger flowing mode (drain)
+    // See: http://sassdoc.com/gulp/#drain-event
+    .resume();
 });
 
 gulp.task('default', ['sass'], function() {
