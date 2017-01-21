12c4218f266327c73d86b8191c96a0c0c8c7a6a5
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-01-20 22:09:33 -0800

    js require once

--- themes/custom/GMH/js/gmh.js
+++ themes/custom/GMH/js/gmh.js
@@ -8,40 +8,45 @@
   /**
    * Use this behavior as a template for custom Javascript.
    */
-  // Drupal.behaviors.equalHeight_behavior = {
-  //   attach: function (context, settings) {
-  //     // MatchHeights
-  //     $('.equalHeight').matchHeight();
-  //   }
-  // };
-  // Drupal.behaviors.slickNav_behavior = {
-  //   attach: function (context, settings) {
-  //     // SlickNav
-  //     $('.menu').slicknav({
-  //       duration: 1500,
-  //       easingOpen: "easeOutBounce"
-  //     });
-  //   }
-  // };
-  // Drupal.behaviors.slickSlider_behavior = {
-  //   attach: function (context, settings) {
-  //     // Slick Slider
-  //     // Single Item no dots
-  //     $('.single-item').slick({
-  //       dots: false,
-  //       arrows: true,
-  //       slidesToShow: 1,
-  //       fade: true
-  //     });
-  //     // Single Item w/ dots
-  //     $('.single-item-dots').slick();
-  //   }
-  // };
-  // Drupal.behaviors.pageHero_behavior = {
-  //   attach: function (context, settings) {
-  //     $("#gmh_page_hero")
-  //       .appendTo("#gmh_page_hero_div");
-  //   }
-  // };
+  Drupal.behaviors.equalHeight = {
+    attach: function (context, settings) {
+      // MatchHeights
+      $('.equalHeight').matchHeight();
+    }
+  };
+  Drupal.behaviors.slickNav = {
+    attach: function (context, settings) {
+      context.once(function() {
+        // SlickNav
+        $('.menu').slicknav({
+          duration: 1500,
+          easingOpen: "easeOutBounce"
+        });
+      }
+    }
+  };
+  Drupal.behaviors.slickSlider = {
+    attach: function (context, settings) {
+      context.once(function() {
+        // Slick Slider
+        // Single Item no dots
+        $('.single-item').slick({
+          dots: false,
+          arrows: true,
+          slidesToShow: 1,
+          fade: true
+        });
+        // Single Item w/ dots
+        $('.single-item-dots').slick();
+      }
+    }
+  };
+  Drupal.behaviors.pageHero = {
+    attach: function (context, settings) {
+      context.once(function() {
+        $("#gmh_page_hero").appendTo("#gmh_page_hero_div");
+      }
+    }
+  };
 
 })(jQuery, Drupal);
