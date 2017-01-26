6a142185fa7edb07b7901b0a90d1e83d22b750ee
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-01-26 01:57:25 -0800

    .

--- themes/custom/GMH/js/gmh.js
+++ themes/custom/GMH/js/gmh.js
@@ -8,10 +8,9 @@
   /**
    * Use this behavior as a template for custom Javascript.
    */
-  Drupal.behaviors.gmh = {
+  Drupal.behaviors.matchHeight = {
     attach: function (context, settings) {
-      // MatchHeights
-      // $('.equalHeight').matchHeight();
+      $('.equalHeight').matchHeight();
       // Page header moves
       // $("#gmh_page_hero").appendTo("#gmh_page_hero_div");
       //Slick Slider
@@ -27,39 +26,5 @@
       // $('.menu').slicknav();
     }
   };
-  // Drupal.behaviors.slickNav = {
-  //   attach: function (context, settings) {
-  //       // SlickNav
-  //       $('.menu').slicknav({
-  //         // label: '',
-  //         // duration: 1000,
-  //         // easingOpen: "easeOutBounce", //available with jQuery UI
-  //         appendTo:'#brand'
-  //       });
-  //   }
-  // };
-  // Drupal.behaviors.slickSlider = {
-  //   attach: function (context, settings) {
-  //     context.once(function() {
-  //       // Slick Slider
-  //       // Single Item no dots
-  //       $('.single-item').slick({
-  //         dots: false,
-  //         arrows: true,
-  //         slidesToShow: 1,
-  //         fade: true
-  //       });
-  //       // Single Item w/ dots
-  //       $('.single-item-dots').slick();
-  //     }
-  //   }
-  // };
-  // Drupal.behaviors.pageHero = {
-  //   attach: function (context, settings) {
-  //     context.once(function() {
-  //       $("#gmh_page_hero").appendTo("#gmh_page_hero_div");
-  //     });
-  //   }
-  // };
 
 })(jQuery, Drupal);
