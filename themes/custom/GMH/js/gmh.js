94def5a9e3bd1f52d77157f3922b3b85f392a680
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-01-26 02:16:49 -0800

    .

--- themes/custom/GMH/js/gmh.js
+++ themes/custom/GMH/js/gmh.js
@@ -8,28 +8,58 @@
   /**
    * Use this behavior as a template for custom Javascript.
    */
-  Drupal.behaviors.equalHeight_behavior = {
+  Drupal.behaviors.gmh = {
     attach: function (context, settings) {
       // MatchHeights
       $('.equalHeight').matchHeight();
+      // Page header moves
+      // $("#gmh_page_hero").appendTo("#gmh_page_hero_div");
+      //Slick Slider
+      // Single Item no dots
+      // $('.single-item').slick({
+      //   // dots: true,
+      //   arrows: true
+      //   // slidesToShow: 1,
+      //   // fade: true
+      // });
+      $('.single-item').slick();
       // SlickNav
-      $('.menu').slicknav({
-		  label: '',
-		  duration: 1000,
-		  easingOpen: "easeOutBounce", //available with jQuery UI
-		  prependTo:'#demo2'
-	  });
+      $('.menu').slicknav();
     }
   };
+  // Drupal.behaviors.slickNav = {
+  //   attach: function (context, settings) {
+  //       // SlickNav
+  //       $('.menu').slicknav({
+  //         // label: '',
+  //         // duration: 1000,
+  //         // easingOpen: "easeOutBounce", //available with jQuery UI
+  //         appendTo:'#brand'
+  //       });
+  //   }
+  // };
+  // Drupal.behaviors.slickSlider = {
+  //   attach: function (context, settings) {
+  //     context.once(function() {
+  //       // Slick Slider
+  //       // Single Item no dots
+  //       $('.single-item').slick({
+  //         dots: false,
+  //         arrows: true,
+  //         slidesToShow: 1,
+  //         fade: true
+  //       });
+  //       // Single Item w/ dots
+  //       $('.single-item-dots').slick();
+  //     }
+  //   }
+  // };
+  // Drupal.behaviors.pageHero = {
+  //   attach: function (context, settings) {
+  //     context.once(function() {
+  //       $("#gmh_page_hero").appendTo("#gmh_page_hero_div");
+  //     });
+  //   }
+  // };
 
 })(jQuery, Drupal);
-
-
-// $(function() {
-//     $('.equalHeight').matchHeight();
-// });
-
-// // SlickNav
-// $(function(){
-//     $('#menu').slicknav();
-// });
