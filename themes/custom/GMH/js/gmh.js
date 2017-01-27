4604a84ef91d2143332481e4fe514d8941f64828
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-01-27 00:49:07 -0800

    ;

--- themes/custom/GMH/js/gmh.js
+++ themes/custom/GMH/js/gmh.js
@@ -10,36 +10,36 @@
    */
   Drupal.behaviors.gmh = {
     attach: function (context, settings) {
+
       // MatchHeights
       $('.equalHeight').matchHeight();
+
       // Page header moves
       $("#gmh_page_hero").appendTo("#gmh_page_hero_div");
+
       //Slick Slider
-      // Single Item no dots
-      // $('.single-item').slick({
-      //   // dots: true,
-      //   arrows: true
-      //   // slidesToShow: 1,
-      //   // fade: true
-      // });
       $('.single-item').slick({
-          arrows: true
+          arrows: true,
+          autoplay: true,
+          prevArrow: "<button type="button" class="slick-prev">Previous</button>",
+          nextArrow: "<button type="button" class="slick-prev">Next</button>,"
+          dots: true,
+          mobileFirst: true
       });
-      // SlickNav
-      // $('.menu').slicknav();
+
     }
   };
-  // Drupal.behaviors.slickNav = {
-  //   attach: function (context, settings) {
-  //       // SlickNav
-  //       $('.menu').slicknav({
-  //         // label: '',
-  //         // duration: 1000,
-  //         // easingOpen: "easeOutBounce", //available with jQuery UI
-  //         appendTo:'#brand'
-  //       });
-  //   }
-  // };
+  Drupal.behaviors.slickNav = {
+    attach: function (context, settings) {
+      // SlickNav
+      $('.menu').slicknav({
+        label: '',
+        // duration: 1000,
+        // easingOpen: "easeOutBounce", //available with jQuery UI
+        appendTo:'#brand'
+      });
+    }
+  };
   // Drupal.behaviors.slickSlider = {
   //   attach: function (context, settings) {
   //     context.once(function() {
