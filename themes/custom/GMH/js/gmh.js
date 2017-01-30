0fc70c63919f3e744ef32ef725d37f950c74b4c5
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-01-29 21:56:15 -0800

    menu

--- themes/custom/GMH/js/gmh.js
+++ themes/custom/GMH/js/gmh.js
@@ -22,7 +22,7 @@
       $("#gmh_page_hero").appendTo("#gmh_page_hero_div");
 
       // SlickNav
-      $('.menu').slicknav({
+      $('ul.menu').slicknav({
         label: '',
         // duration: 1000,
         // easingOpen: "easeOutBounce", //available with jQuery UI
