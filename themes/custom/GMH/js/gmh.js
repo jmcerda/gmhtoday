7b6881e59be8903a0d63004dd8a1e7a89b252768
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-02-09 20:07:11 -0800

    .

--- themes/custom/GMH/js/gmh.js
+++ themes/custom/GMH/js/gmh.js
@@ -15,9 +15,9 @@
           // arrows: true
       });
       $('.ads').slick({
-          autoplay: true,
-          // dots: true,
-          // infinite: true,
+          // autoplay: true,
+          dots: true,
+          infinite: true,
           slidesToShow: 3,
           slidesToScroll: 3
       });
