fc58ef7be41f9849f182c1e8129cc8539cbaab88
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2017-02-09 18:52:10 -0800

    .

--- themes/custom/GMH/js/gmh.js
+++ themes/custom/GMH/js/gmh.js
@@ -18,7 +18,7 @@
           autoplay: true,
           autoplaySpeed: 5000,
           dots: true,
-          infinite: true,
+          // infinite: true,
           // slidesToShow: 3,
           // slidesToScroll: 3,
           variableWidth: true
